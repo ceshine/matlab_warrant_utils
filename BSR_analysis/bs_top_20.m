e4fedfe0c8ccafe5dbd99045ed2cb524e43be8fe
ceshine <ceshine@ceshine.net>
2012-08-07 12:05:11 +0800

    Simple Buy/Sell Report Analysis

---
+++ BSR_analysis/bs_top_20.m
@@ -0,0 +1,27 @@
+symbol = input('\nSymbol:  ');
+from_d = input('\nFrom Date: ', 's');
+to_d = input('\nUntil Date: ', 's');
+query_template = [ 'SELECT broker_id, name, tbuy, tsell, net ' ...
+          'FROM (SELECT broker_id , SUM( buy)  AS tbuy, SUM( sell ) AS tsell , SUM( buy ) - SUM(sell ) AS net ' ...
+                'FROM  bsreport ' ...
+                'WHERE  company_id = %d AND date >= "%s" AND date <= "%s" ' ...
+                'GROUP BY  broker_id ' ...
+                'ORDER BY net %s ' ...
+                'LIMIT 20) AS a '  ...
+            'JOIN brokers ON a.broker_id = brokers.id' ];
+ 
+scrsz = get(0, 'ScreenSize');
+figure('Position', [50 100 scrsz(3)*0.9 scrsz(4)*0.8]);        
+hold on
+
+query = sprintf(query_template, symbol, from_d, to_d, 'ASC');
+result = sql_query(query);
+subplot(2,1,1);
+bsr_bybroker_chart([cell2mat(result(:,3)), cell2mat(result(:,4))*-1, cell2mat(result(:,5))], result(:,2), 'SELLERS');
+
+query = sprintf(query_template, symbol, from_d, to_d, 'DESC');
+result = sql_query(query);
+subplot(2,1,2);
+bsr_bybroker_chart([cell2mat(result(:,3)), cell2mat(result(:,4))*-1, cell2mat(result(:,5))], result(:,2), 'BUYERS');
+
+hold off
---
+++ BSR_analysis/sql_query.m
@@ -0,0 +1,25 @@
+function [ result ] = sql_query( query )
+%SQL_QUERY Summary of this function goes here
+%   Detailed explanation goes here
+    if (exist('conn', 'var') == 0 || isconnection(conn) ~= 1),
+        bsr_db
+    else
+        try
+            ping(conn)
+        catch err
+            bsr_db
+        end
+    end
+    
+    for i = 1:2,
+        try
+            result = fetch(conn, query);
+            return
+        catch err
+            err.message
+            bsr_db
+        end
+    end
+    
+end
+
