879cb1060d348190ac3e3b877bc479a8e37bcdd7
ceshine <ceshine@ceshine.net>
2012-08-08 10:31:31 +0800

    bs top 20 UI update

--- BSR_analysis/bs_top_20.m
+++ BSR_analysis/bs_top_20.m
@@ -14,14 +14,16 @@ scrsz = get(0, 'ScreenSize');
 figure('Position', [50 100 scrsz(3)*0.9 scrsz(4)*0.8]);        
 hold on
 
-query = sprintf(query_template, symbol, from_d, to_d, 'ASC');
-result = sql_query(query);
-subplot(2,1,1);
-bsr_bybroker_chart([cell2mat(result(:,3)), cell2mat(result(:,4))*-1, cell2mat(result(:,5))], result(:,2), 'SELLERS');
-
 query = sprintf(query_template, symbol, from_d, to_d, 'DESC');
 result = sql_query(query);
+subplot(2,1,1);
+title_s = sprintf('Buyers for %d from %s to %s', symbol, from_d, to_d);
+bsr_bybroker_chart([cell2mat(result(:,3)), cell2mat(result(:,4))*-1, cell2mat(result(:,5))], result(:,2), title_s);
+
+query = sprintf(query_template, symbol, from_d, to_d, 'ASC');
+result = sql_query(query);
+title_s = sprintf('Sellers for %d from %s to %s', symbol, from_d, to_d);
 subplot(2,1,2);
-bsr_bybroker_chart([cell2mat(result(:,3)), cell2mat(result(:,4))*-1, cell2mat(result(:,5))], result(:,2), 'BUYERS');
+bsr_bybroker_chart([cell2mat(result(:,3)), cell2mat(result(:,4))*-1, cell2mat(result(:,5))], result(:,2), title_s);
 
 hold off
