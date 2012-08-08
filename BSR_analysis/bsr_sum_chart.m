d7c20b0adca2ddbed92f81a11822431ec19119e2
ceshine <ceshine@ceshine.net>
2012-08-08 21:05:29 +0800

    Add Net Bar and Allow N input

--- BSR_analysis/bsr_sum_chart.m
+++ BSR_analysis/bsr_sum_chart.m
@@ -1,11 +1,12 @@
 function [ output_args ] = bsr_sum_chart( tbuy, tsell, ddate, title_s)
 %BSR_SUM_CHART Summary of this function goes here
 %   Detailed explanation goes here
-    bsn = [ tbuy tsell*-1] / 1000;
+    bsn = [ tbuy tsell*-1 tbuy+tsell] / 1000;
     
     h = bar(bsn);
     set(h(1), 'facecolor', 'red')
     set(h(2), 'facecolor', 'green')
+    set(h(3), 'facecolor', 'yellow')
     set(gca,'XTick',1:1:size(bsn,1))
     set(gca,'XTickLabel', datestr(datenum(ddate), 'mmdd'))
     xlim([0 size(bsn,1)+1]);
