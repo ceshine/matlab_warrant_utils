629463677032c626e8925d2ff3f56d57d888b701
ceshine <ceshine@ceshine.net>
2012-08-09 09:40:40 +0800

    rename scripts and supress debug message

--- BSR_analysis/bsr_db.m
+++ BSR_analysis/bsr_db.m
@@ -1,4 +1,4 @@
 % slCharacterEncoding('UTF-8');
 % feature('DefaultCharacterSet', 'UTF8');
 load('db_account.mat'); 
-conn = database('', db_user, db_password, 'com.mysql.jdbc.Driver', db_url)
+conn = database('', db_user, db_password, 'com.mysql.jdbc.Driver', db_url);
