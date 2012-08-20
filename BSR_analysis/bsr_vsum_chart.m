a2af638ee9a875394443d9eadf63b2777498461f
CeShine Lee <ceshine@ceshine.net>
2012-08-20 11:48:14 +0800

    Volume Info Added sum15/sum5

---
+++ BSR_analysis/bsr_vsum_chart.m
@@ -0,0 +1,17 @@
+function [ output_args ] = bsr_sum_chart( tbuy, tsell, volume, ddate, title_s)
+%BSR_SUM_CHART Summary of this function goes here
+%   Detailed explanation goes here
+    bsn = [ tbuy tsell*-1 tbuy+tsell tbuy-tsell volume] / 1000;
+    
+    h = bar(bsn);
+    set(h(1), 'facecolor', 'red')
+    set(h(2), 'facecolor', 'green')
+    set(h(3), 'facecolor', 'yellow')
+    set(gca,'XTick',1:1:size(bsn,1))
+    set(gca,'XTickLabel', datestr(datenum(ddate), 'mmdd'))
+    xlim([0 size(bsn,1)+1]);
+    
+    title(title_s);
+
+end
+
---
+++ BSR_analysis/vdsum5.m
@@ -0,0 +1,22 @@
+symbol = input('\nSymbol:  ');
+
+%from_d = input('\nFrom Date: ', 's');
+%to_d = input('\nUntil Date: ', 's');
+query_template = [ 'SELECT ddate, tbuy, tsell, volume '...
+                    'FROM (SELECT date as ddate,tbuy, tsell FROM '...
+                    '(SELECT * FROM (SELECT SUM(tnet) as tbuy,date FROM bstop5 WHERE tnet > 0 AND company_id = %d GROUP BY date) as bbuy '...
+                    'JOIN (SELECT SUM(tnet) as tsell,date as ddate FROM bstop5 WHERE tnet < 0 AND company_id = %d GROUP BY date) as bsell ON bbuy.date = bsell.ddate) AS a) AS b '...
+                    'JOIN bsvolume '...
+                    'ON bsvolume.company_id = %d AND bsvolume.date = b.ddate' ];
+ 
+scrsz = get(0, 'ScreenSize');
+figure('Position', [50 100 scrsz(3)*0.5 scrsz(4)*0.5]);        
+
+hold on
+
+query = sprintf(query_template, symbol, symbol, symbol);
+result = sql_query(query);
+title_s = sprintf('Daily Top 5/5 ¥D¤O for %d', symbol);
+bsr_vsum_chart(cell2mat(result(:,2)), cell2mat(result(:,3)), cell2mat(result(:,4)), cell2mat(result(:,1)), title_s)
+
+hold off
