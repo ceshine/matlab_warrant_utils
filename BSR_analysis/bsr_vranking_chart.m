1ae2a46a5c86cc753f34b2f84fd0c13907a97047
CeShine Lee <shuanck@gmail.com>
2013-01-15 20:51:33 +0800

    Rank only spots of warrants, scale by volume

--- BSR_analysis/bsr_vranking_chart.m
+++ BSR_analysis/bsr_vranking_chart.m
@@ -1,7 +1,9 @@
 function [ output_args ] = bsr_vranking_chart( tbuy, tsell, tnet, volume, lab, title_s )
 %BSR_RANKING_CHART Summary of this function goes here
 %   Detailed explanation goes here
-    bsn = [ tbuy tsell*-1 tnet volume] / 1000;
+    mvol = max(volume);
+    bsn = [ tbuy tsell*-1 tnet tbuy-tsell volume] / 1000 ./ repmat((volume*mvol), 1, 5);
+    
     
     h = bar(bsn);
     set(h(1), 'facecolor', 'red')
